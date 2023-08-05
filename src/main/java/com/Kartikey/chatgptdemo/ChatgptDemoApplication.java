c33f7199d7213be971c5d9cb97eb6f8e06e2691c
Kartikk1127 <kartikey.srivastava1271@gmail.com>
2023-08-06 00:04:54 +0530

    comment add

--- src/main/java/com/Kartikey/chatgptdemo/ChatgptDemoApplication.java
+++ src/main/java/com/Kartikey/chatgptdemo/ChatgptDemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//this is a chat gpt application
 public class ChatgptDemoApplication {
 
 	public static void main(String[] args) {
